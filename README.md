e0462fed69688d3c1721597224e26c45110a484e
studiozut <studiozut@gmail.com>
2015-08-04 14:56:29 -0400

    changes to widest view size and some adjustments by Phil, as well as some file cleanup

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 Custom home page for GW Libraries Omeka instance
 ------------------------------------------------
 
-A portal for our Omeka instance using Omeka APIs to expose content like newly added items and featured exhibits combined with other online collections and content outside of Omeka 
+A portal for our Omeka instance using Omeka APIs to expose content like newly added items and featured exhibits combined with other online collections and content housed outside of Omeka.
+
+Configuration:
+
+Move files to a directory in root called "portal"
+
+Set .htaccess to redirect root traffic to /portal/index.php
+```
+# redirect to root traffic from /var/www/index.php to /portal/index.php (custom portal) 
+RedirectMatch 303 ^/?$ /portal/index.php
+```
--- index.php
+++ index.php
@@ -2,22 +2,25 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>GW Libraries Exhibits</title><!-- your page title -->
+    <title>Exhibits at the GW Libraries</title><!-- your page title -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- important! -->
     <meta name="description" content=""><!-- optional -->
     <meta name="author" content=""><!-- optional -->
         
     <!-- styles -->
-    <!-- these are the stylesheets: first two grab the Bootstrap cdn and Font Awesome! cdn stylesheets, the other two are for the Library theme -->
+    <!-- these are the stylesheets: the first two grab the Bootstrap cdn and Font Awesome! cdn stylesheets, the next two are for the Library bootswatch, followed by the slideshow css. Last is the "portal" css -->
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet" media="all">
     <!-- Font Awesome! http://fontawesome.io/get-started -->
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="http://library.gwu.edu/app-assets/libheader/2_002/css/libheader7_lite.css" rel="stylesheet" media="all">
-    <link href="http://library.gwu.edu/app-assets/libheader/2_002/css/libheader7_lite_bootswatch.css" rel="stylesheet" media="all">
+    <link href="http://gwappassets-prod.wrlc.org/app-assets/libheader/2_002/css/libheader7_lite.css" rel="stylesheet" media="all">
+    <link href="http://gwappassets-prod.wrlc.org/app-assets/libheader/2_002/css/libheader7_lite_bootswatch.css" rel="stylesheet" media="all">
     <!-- basic-slider css -->
-    <link href="bjqs.css" rel="stylesheet" />
+    <link href="css/bjqs.css" rel="stylesheet" />
     <!-- omeka-home custom css -->
-    <link href="omeka-home.css" rel="stylesheet" />
+    <link href="css/omeka-home.css" rel="stylesheet" />
+    <!-- Google font css -->
+    <link href='http://fonts.googleapis.com/css?family=BenchNine:400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,400italic' rel='stylesheet' type='text/css'>
 
     <!-- js (some js links are at the bottom of the page) -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
@@ -26,7 +29,7 @@
 
     <!-- IE css mods and HTML5 shim, for IE6-8 support of HTML5 elements --><!-- important! -->
     <!--[if lt IE 9]>
-    <link href="http://library.gwu.edu/app-assets/libheader/2_002/css/libheader7_lite_ie.css" rel="stylesheet">
+    <link href="http://gwappassets-prod.wrlc.org/app-assets/libheader/2_002/css/libheader7_lite_ie.css" rel="stylesheet">
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
@@ -48,8 +51,8 @@
     </script>
                                 
     <!-- Fav and touch icons -->
-    <link rel="shortcut icon" href="http://library.gwu.edu/app-assets/libheader/2_002/favicon.ico">
-    <link rel="apple-touch-icon" href="http://library.gwu.edu/app-assets/libheader/2_002/img/apple-touch-icon-iphone.png"><!-- optional, and this is just the min version -->
+    <link rel="shortcut icon" href="http://gwappassets-prod.wrlc.org/app-assets/libheader/2_002/favicon.ico">
+    <link rel="apple-touch-icon" href="http://gwappassets-prod.wrlc.org/app-assets/libheader/2_002/img/apple-touch-icon-iphone.png"><!-- optional, and this is just the min version -->
 
   </head> 
 
@@ -58,7 +61,7 @@
     <header>
     	<div id="header-container">
         <div id="header-contents">
-    	    <img alt="header image for the Exhibits page" src="header-image.png" />
+    	    <img alt="header image for the Exhibits page" src="assets/header-image.png" />
         </div>
       </div>
     </header>
@@ -71,15 +74,16 @@
         <div class="span12" id="slider-row"> 
           <div style="margin:0 auto;" id="slider">
 	          <ul class="bjqs">
-              <!-- title is used as caption -->
-		          <li><img src="masthead-slideshow-placeholder.png" alt="The PNC-RIGGS Collection" title="The PNC-RIGGS Collection" /></li>
-		          <li><img src="masthead-slideshow-placeholder-2.png" alt="alt tag here" title="The Test Collection" /></li>
-              <li><img src="masthead-slideshow-placeholder-3.png" alt="alt tag here" title="The Monument Collection" /></li>
+              <!-- IMAGES FOR SLIDESHOW. There can be more than three. Title is used as caption -->
+		          <li><img src="assets/masthead-slideshow-placeholder.png" alt="The PNC-RIGGS Collection" title="The PNC-RIGGS Collection" /></li>
+		          <li><img src="assets/masthead-slideshow-placeholder-2.png" alt="alt tag here" title="The Test Collection" /></li>
+              <li><a href="http://exhibits.library.gwu.edu/exhibits/show/digitaldc"><img src="assets/DDC_slide.jpg" alt="alt tag here" title="Digital DC: A Community History Project" /></a></li>
 	          </ul>
           </div>
         </div>
       </div>
 
+      <!-- basic-slider controls. There are more options available: github.com/jcobb/basic-jquery-slider/ -->
       <script>
       jQuery(document).ready(function($) {
       	$('#slider').bjqs({
@@ -96,16 +100,16 @@
 
       <div class="row-fluid nav-items content-1" id="nav-items">
         <div class="span2">
-          <a href="http://exhibits.library.gwu.edu/about" title="more information about GW Libraries Exhibits">ABOUT US</a>	
+          <a href="about.html" title="more information about GW Libraries Exhibits">ABOUT US</a>	
         </div>
         <div class="span2">
-          <a href="https://library.gwu.edu/collections/exhibits" title="see our online Exhibits">EXHIBITS</a>
+          <a href="http://exhibits.library.gwu.edu/exhibits" title="see our online Exhibits">EXHIBITS</a>
         </div>
         <div class="span2">
-          <a href="#" title="see our schedule of upcoming events">CALENDAR</a>
+          <a href="schedule.html" title="see our schedule of upcoming events">CALENDAR</a>
         </div>
         <div class="span2">
-          <a href="http://library.gwu.edu/scrc/collections" title="see our online Collections">COLLECTIONS</a>
+          <a href="http://exhibits.library.gwu.edu/collections/browse" title="see our online Collections">COLLECTIONS</a>
         </div>
         <div class="span2 nav-items nav-items-items">
           <a href="http://exhibits.library.gwu.edu/items/browse" title="browse items in our collections">ITEMS</a>
@@ -125,12 +129,12 @@
 	    <div id="intro-content-container" class="row-fluid">
 	      <div class="span5">
   		    <div class="content-block">
-  	        <p class="overview-left-text">Donec suspendisse ac, erat nec hac quam vitae, nibh sapien, magnis tortor ut et vitae elementum enim. Tincidunt vel sed augue praesent consequat magna, aenean eu orci. Pellentesque dictum nibh nibh id, molestie non, turpis id vestibulum nunc amet lectus, tortor amet placerat, illo mollis scelerisque malesuada vitae sed.</p>
+  	        <p class="overview-left-text">The GW Libraries Exhibits Program showcases and celebrates the libraries' strengths as a learning center; its role in supporting research and teaching at GW; its many local, national and international collections; and the research topics pursued by its staff. Exhibits are frequently displayed in conjunction with academic lectures, Library functions, collections receptions and related events.</p>
   		    </div>
         </div>
         <div class="span5">
       		<div class="content-block">
-      		  <p class="overview-right-text">Nisl nonummy. Feugiat est nibh rhoncus auctor, felis ligula aliquam amet condimentum, amet lobortis vitae quam id. Iaculis placerat vitae, porttitor ac ornare quis. Libero eu varius et vivamus est sapien, porttitor lobortis diam a sodales, nisl adipiscing vel ac, risus dui platea neque condimentum.</p>
+      		  <p class="overview-right-text">Drawing on the varied collections of the Library’s Collections units—the Special Collections Research Center, the Labor History Research Center, The I. Edward Kiev Judaica Collection, the Global Resources Center— The Exhibits portal page provides researchers with a wealth of scanned texts and primary documents, curated by Library subject matter experts.</p>
       		</div>
         </div>
           <div class="span2">
@@ -145,7 +149,7 @@
 	          <ul id="collection-list">
 
               <?php
-              // collections
+              // COLLECTIONS
 
               $json_string = 'http://exhibits.library.gwu.edu/api/collections';
               $jsondata = file_get_contents($json_string);
@@ -172,32 +176,26 @@
         		<ul id="items-list">
 
               <?php
-              // items
+              // ITEMS
 
               $headerArray = get_headers($url, 1);
-              // echo "<p style='margin-top:2em;'>header total results: " . $headerArray['Omeka-Total-Results'] . "</p>";
 
               $howMany = ceil($headerArray['Omeka-Total-Results'] / 50);
 
               $json_string = "http://exhibits.library.gwu.edu/api/items?page=" . $howMany;
               $jsondata = file_get_contents($json_string);
-              //$obj = json_decode($jsondata,true);
 
               $itemArray  = json_decode($jsondata, true); // decode JSON to associative array
               $itemArrayDesc = array_reverse($itemArray);
 
-              //echo "<p>Total Items = " . $headerArray['Omeka-Total-Results'] . " which means " . $howMany . " pages (sets of 50)</p>";
-
               $i = 0;
               foreach($itemArrayDesc as $info){
-                //echo "<div style='margin-bottom:3em;'>";
 
                 $theVar = $info['files']['url'];
                 $json_string2 = $theVar;
                 $jsondata2 = file_get_contents($json_string2);
                 $itemArray2  = json_decode($jsondata2, true);
                   foreach($itemArray2 as $info2){
-                    //echo "<div style='margin:.5em .5em 0;'><img style='border:1px solid #ccc;' src='" . $info2['file_urls']['thumbnail'] . "'></div><br />";
                   }
                 echo "<li><a href='http://exhibits.library.gwu.edu/items/show/" . $info['id']  . "'>" . $info['element_texts'][0]['text'] . "</a></li>";
 
@@ -216,28 +214,28 @@
         </div>
 
         <div class="span6 showcase-column">
-      		<img alt="featured exhibit splash image" id="featured-exhibit-image" src="featured-placeholder.png" />
+      		<img alt="featured exhibit splash image" id="featured-exhibit-image" src="assets/featured-placeholder.png" />
           <div class="content-block">
             <h2>Hear in DC: The DC Vernacular Music Archive</h2>
-            <p>Feugiat est nibh rhoncus auctor, felis ligula aliquam amet condimentum, amet lobortis vitae quam id. Iaculis placerat vitae, porttitor ac ornare quis. Libero eu varius et vivamus est sapien, porttitor lobortis diam a sodales, nisl adipiscing vel ac, risus dui platea neque condimentum. <a href="http://library.gwu.edu/news-events/posts/hear-dc-vernacular-music-nations-capital" aria-label="more about the Hear in DC exhibit">read more</a></p>
+            <p class="overview-left-text">Are you a fan of D.C.'s homegrown hardcore punk scene, or the late great Chuck Brown? Come to the 7th floor of Gelman to see the exhibit "Hear in DC: Vernacular Music in the Nation's Capital," featuring historic photos, LPs, and promotional materials for bands that got their start in D.C. The exhibit includes folk, blues, bluegrass, punk, and go-go musicians, featuring items from performers such as Mississippi John Hurt, Flatt and Scruggs, and Fugazi, all part of the new D.C. Vernacular Music Archive at GW. <a href="http://library.gwu.edu/news-events/posts/hear-dc-vernacular-music-nations-capital" aria-label="more about the Hear in DC exhibit">read more</a></p>
             <h3 class="current-exhibits-link"><a href="#">CURRENTLY INSTALLED EXHIBITS</a><h3>
       		</div>
         </div>
 
         <div class="span3 showcase-column-3">
-      		<div class="content-block">
+      		<div class="content-block-right">
             <h2>ONLINE EXHIBITS</h2>
             <div>
-              <img alt="Hebrew Printing in Ukraine splash image" src="exhibit-hebrew.png" />
+              <img alt="Hebrew Printing in Ukraine splash image" src="assets/exhibit-hebrew.png" />
               <p class="online-exhibit-title">Hebrew Printing in Ukraine</p>
-              <p class="online-exhibit-text">Iaculis placerat vitae, porttitor ac ornare quis. Libero eu varius et vivamus est sapien. <a aria-label="more about the Hebrew Printing exhibit" href="#">read more</a></p>
+              <p class="online-exhibit-text">A geographic and typographic panorama of every region of this cultural crossroads, including ancient, medieval and modern texts printed over centuries in diverse territories. <a aria-label="http://exhibits.library.gwu.edu/ukraineprinting/" href="#">read more</a></p>
             </div>
             <div>
-              <img alt="Presidential Presence splash image" src="exhibit-president.png" />
-              <p class="online-exhibit-title">Presidential Presence</p>
-              <p class="online-exhibit-text">Libero eu varius et vivamus est sapien, porttitor lobortis diam a sodales, nisl adipiscing vel ac, risus dui platea neque condimentum. <a aria-label="more about the Presidential Presence exhibit" href="#">read more</a></p>
+              <img alt="CWAC" src="assets/Stowell.png" />
+              <p class="online-exhibit-title">City Within a City: The Kerry Stowell Collection</p>
+              <p class="online-exhibit-text">An overview of the storied residence's inhabitants, history and culture, from The Kerry Stowell Collection. <a aria-label="more about City Within a City" href="http://exhibits.library.gwu.edu/stowell/index.html">read more</a></p>
             </div>
-              <h3 class="all-online-exhibits-link"><a href="#">SEE ALL ONLINE EXHIBITS</a><h3>
+              <h3 class="all-online-exhibits-link"><a href="http://exhibits.library.gwu.edu/exhibits">SEE ALL ONLINE EXHIBITS</a><h3>
 		      </div>
         </div>
       </div>
@@ -252,11 +250,11 @@
           <section class="pane clear" id="local-footer"> 
 
             <div id="logo-container">
-              <img alt="logo: GW Libraries" src="gw_iddol_libraries_wht_rev_resized.png">
+              <img alt="logo: GW Libraries" src="assets/gw_iddol_libraries_wht_rev_resized.png">
             </div>
 
 	          <div class="footer-lib-logo">
-		          <h2>GW Libraries</h2>
+		          <h3>GW Libraries</h3>
 	          </div>
 	
 	          <article class="address">
@@ -293,7 +291,7 @@
       </div>
     </footer><!-- end portal footer -->
         
-    <!-- javascript placed at the end of the document so the pages load faster -->
+    <!-- javascript placed at the end of the document so page loads faster -->
     <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script><!-- important -->
         
   </body>
